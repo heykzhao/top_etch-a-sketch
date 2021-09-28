3b716c87beab246be83a8dd1065a412220d3fb8d
Kevin Zhao <72359861+heykzhao@users.noreply.github.com>
2021-09-28 19:53:32 -0400

    Create hover effect using JS event listeners

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
-let squareWidth = 25;
+let squareWidth = 50;
 let squareTotal = squareWidth * squareWidth;
-const etchASketchGrid = document.querySelector("#etch-a-sketch-container");
+
 const etchASketchGridSize = document.getElementById("etch-a-sketch-container");
 etchASketchGridSize.style.setProperty("--gridSize", squareWidth);
 
+const etchASketchGrid = document.querySelector("#etch-a-sketch-container");
 for (let i = 0; i < squareTotal; i+=1) {
-    const etchASketchBlock = document.createElement("div");
-    etchASketchBlock.className += "block";
-    etchASketchGrid.appendChild(etchASketchBlock)
+    const initialBlock = document.createElement("div");
+    initialBlock.className += "block";
+    etchASketchGrid.appendChild(initialBlock)
 }
+
+function addColor(e) {
+    e.target.classList.add("hovered");
+};
+
+const etchASketchBlocks = document.querySelectorAll(".block");
+etchASketchBlocks.forEach(etchASketchBlock => etchASketchBlock.addEventListener("mouseover",addColor));

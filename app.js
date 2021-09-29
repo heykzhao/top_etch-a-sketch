a04179c1a98753ce4f85d104b525d3534f135493
Kevin Zhao <72359861+heykzhao@users.noreply.github.com>
2021-09-29 19:48:52 -0400

    Add clear screen functionality via button, which is also triggered by shortcut " c "

--- app.js
+++ app.js
@@ -15,5 +15,17 @@ function addColor(e) {
     e.target.classList.add("hovered");
 };
 
+function clearBoard() {
+    etchASketchBlocks.forEach(etchASketchBlock => etchASketchBlock.classList.remove("hovered"))
+};
+
 const etchASketchBlocks = document.querySelectorAll(".block");
 etchASketchBlocks.forEach(etchASketchBlock => etchASketchBlock.addEventListener("mouseover",addColor));
+
+//keyboard shortcuts
+const clearBoardButton = document.querySelector("#clear-board-button");
+document.onkeyup = function(e){
+    if (e.key == "c") {
+        clearBoardButton.click();
+    };
+}

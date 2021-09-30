24b4145d7067e8be4b4ad81e1f1c68663073da6d
Kevin Zhao <72359861+heykzhao@users.noreply.github.com>
2021-09-29 21:17:52 -0400

    Start adding basic UI

--- app.js
+++ app.js
@@ -1,15 +1,31 @@
-let squareWidth = 50;
+// Set Initial Grid Size 
+let squareWidth = 32
 let squareTotal = squareWidth * squareWidth;
-
 const etchASketchGridSize = document.getElementById("etch-a-sketch-container");
-etchASketchGridSize.style.setProperty("--gridSize", squareWidth);
+let etchASketchGrid = document.querySelector("#etch-a-sketch-container");
+setGridWidth();
+createAllBlocks();
+startDrawing();
 
-const etchASketchGrid = document.querySelector("#etch-a-sketch-container");
-for (let i = 0; i < squareTotal; i+=1) {
-    const initialBlock = document.createElement("div");
-    initialBlock.className += "block";
-    etchASketchGrid.appendChild(initialBlock)
-}
+// Allow for Grid Size changes
+const squareWidthInput = document.getElementById("squareWidth");
+squareWidthInput.addEventListener("change", resetGrid)
+
+//keyboard shortcuts to initiate clearing of board, start drawing, and stop drawing
+const clearBoardButton = document.querySelector("#clear-board-button");
+document.onkeyup = function(e){
+    if (e.key == "c") {
+        clearBoardButton.click();
+    } else if (e.key == "s") {
+        stopDrawing();
+    } else if (e.key == "d") {
+        startDrawing();
+    }
+};
+
+function setGridWidth() {
+    etchASketchGridSize.style.setProperty("--gridSize", squareWidth);
+};
 
 function addColor(e) {
     e.target.classList.add("hovered");
@@ -27,19 +43,24 @@ function startDrawing() {
     etchASketchBlocks.forEach(etchASketchBlock => etchASketchBlock.addEventListener("mouseover",addColor));
 }
 
-const etchASketchBlocks = document.querySelectorAll(".block");
-
-startDrawing();
-
-
-//keyboard shortcuts
-const clearBoardButton = document.querySelector("#clear-board-button");
-document.onkeyup = function(e){
-    if (e.key == "c") {
-        clearBoardButton.click();
-    } else if (e.key == "s") {
-        stopDrawing();
-    } else if (e.key == "d") {
-        startDrawing();
+function createAllBlocks() {
+    for (let i = 0; i < squareTotal; i+=1) {
+        const initialBlock = document.createElement("div");
+        initialBlock.className += "block";
+        etchASketchGrid.appendChild(initialBlock)
     }
+    etchASketchBlocks = document.querySelectorAll(".block");
+}
+
+function removeAllBlocks() {
+    document.querySelectorAll(".block").forEach(e => e.remove());
+}
+
+function resetGrid(e) {
+    squareWidth = e.target.value;
+    squareTotal = squareWidth * squareWidth;
+    removeAllBlocks();
+    setGridWidth();
+    createAllBlocks();
+    startDrawing();
 };
